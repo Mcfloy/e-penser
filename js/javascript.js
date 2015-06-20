870205cede9f2a5c975655e3ccb8e26c231764cf
Lucas Perreau <lucasperreau@gmail.com>
2015-06-20 15:26:01 +0200

    test

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCcziTK2NKeWtWQ6kB5tmQ8Q&maxResults=5&key=AIzaSyBCt5jbexNc1YbPTx5odURRdOVoxCbgFcc", false);
+xhr.open("GET", "https://www.googleapis.com/youtube/v3/search?order=date&part=id&channelId=UCcziTK2NKeWtWQ6kB5tmQ8Q&maxResults=5&key=AIzaSyBCt5jbexNc1YbPTx5odURRdOVoxCbgFcc", false);
 xhr.send();
 
 if (xhr.readyState == 4 && xhr.status == 200) {
@@ -11,8 +11,6 @@ if (xhr.readyState == 4 && xhr.status == 200) {
 if (json !== undefined) {
 	var lastVideo = json.items[0];
 	console.log(json);
-	$('#title').html(lastVideo.snippet.title);
-	$('#description').html(lastVideo.snippet.description);
 }
 
 var tag = document.createElement('script');
