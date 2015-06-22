14c723c828f55d6c479e76fa3dca3ad44a2a43e7
Lucas Perreau <lucasperreau@gmail.com>
2015-06-22 17:49:54 +0200

    no fix, just test

--- js/javascript.js
+++ js/javascript.js
@@ -46,6 +46,24 @@ if (uploads !== undefined) {
 		console.log(uploads.items[item]);
 		carousel.owl.push({"item": "<span class='item'>" + uploads.items[item].snippet.thumbnails.medium.url + "</span>"});
 	}
+	carousel = {
+	"owl" : [
+	    {"item" : "<span class='item'><h1>1</h1></span>"},
+	    {"item" : "<span class='item'><h1>2</h1></span>"},
+	    {"item" : "<span class='item'><h1>3</h1></span>"},
+	    {"item" : "<span class='item'><h1>4</h1></span>"},
+	    {"item" : "<span class='item'><h1>5</h1></span>"},
+	    {"item" : "<span class='item'><h1>6</h1></span>"},
+	    {"item" : "<span class='item'><h1>7</h1></span>"},
+	    {"item" : "<span class='item'><h1>8</h1></span>"},
+	    {"item" : "<span class='item'><h1>9</h1></span>"},
+	    {"item" : "<span class='item'><h1>10</h1></span>"},
+	    {"item" : "<span class='item'><h1>11</h1></span>"},
+	    {"item" : "<span class='item'><h1>12</h1></span>"},
+	    {"item" : "<span class='item'><h1>13</h1></span>"},
+	    {"item" : "<span class='item'><h1>14</h1></span>"}
+	]
+}
 	console.log(JSON.stringify(carousel));
 	$("#carousel").owlCarousel({
 		jsonPath : carousel,
