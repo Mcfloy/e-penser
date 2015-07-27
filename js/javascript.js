3a0bb20fdb5998373284eae459278be99f659ad3
Lucas Perreau <lucasperreau@gmail.com>
2015-07-27 22:14:59 +0200

    try to fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.googleapis.com/youtube/v3/search?order=date&part=id,snippet&channelId=UCcziTK2NKeWtWQ6kB5tmQ8Q&maxResults=5&key=AIzaSyBCt5jbexNc1YbPTx5odURRdOVoxCbgFcc", false);
+xhr.open("GET", "http://www.googleapis.com/youtube/v3/search?order=date&part=id,snippet&channelId=UCcziTK2NKeWtWQ6kB5tmQ8Q&maxResults=5&key=AIzaSyBCt5jbexNc1YbPTx5odURRdOVoxCbgFcc", false);
 xhr.send();
 
 if (xhr.readyState == 4 && xhr.status == 200) {
@@ -11,11 +11,11 @@ if (xhr.readyState == 4 && xhr.status == 200) {
 if (uploads !== undefined) {
 	var firstItem = uploads.items[0];
 	var xhrVideo = new XMLHttpRequest();
-	xhrVideo.open("GET", "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + firstItem.id.videoId + "&key=AIzaSyBCt5jbexNc1YbPTx5odURRdOVoxCbgFcc", false);
+	xhrVideo.open("GET", "http://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + firstItem.id.videoId + "&key=AIzaSyBCt5jbexNc1YbPTx5odURRdOVoxCbgFcc", false);
 	xhrVideo.send();
 	if (xhrVideo.readyState == 4 && xhrVideo.status == 200) {
 		var videoInformation = JSON.parse(xhrVideo.responseText);
-		console.log(videoInformation.items[0]);
+		console.log(videoInformation);
 		$('#title').html(videoInformation.items[0].snippet.title);
 		$('#description').html(videoInformation.items[0].snippet.description.replace(/\n/g, "<br/>").replace(/\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim, '<a href="$&" target="_blank">$&</a>'));
 	} else {
